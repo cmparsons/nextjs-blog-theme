adfddc059ac3d624558aa65b71f5f59d03747d6e
Christian Parsons <25161410+cmparsons@users.noreply.github.com>
2024-11-05 14:53:46 -0600

    configure visual editor with annotations

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,33 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "nextjs",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: '~0.6.0',
+  nodeVersion: '18',
+  ssgName: 'nextjs',
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ['posts'],
+      models: [
+        {
+          name: 'Post',
+          type: 'page',
+          urlPath: '/posts/{slug}',
+          filePath: 'posts/{slug}.mdx',
+          fields: [
+            { name: 'title', type: 'string', required: true },
+            {
+              name: 'description',
+              type: 'string',
+            },
+            {
+              name: 'date',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
